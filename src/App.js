5440ab9d315e3da38b419e0d8aaebcda4e51c2a6
JerMontoya <jeralyn.haycock@gmail.com>
2025-09-07 16:35:58 -0800

    cart

--- src/App.js
+++ src/App.js
@@ -12,25 +12,62 @@ function App() {
   const [cart, setCart] = useState([]);
 
   function addToCart(book) {
-   setCart([...cart, book])
+    setCart([...cart, { ...book, quantity: 1 }]);
+  }
+
+  function changeQuantity(book, quantity) {
+    setCart(
+      cart.map((item) =>
+        item.id === book.id
+          ? {
+              ...item,
+              quantity: +quantity,
+            }
+          : item
+      )
+    );
+  }
+
+  function removeItem(item) {
+    setCart(cart.filter(book => book.id !== item.id))
+  }
+
+  function numberOfItems() {
+    let counter = 0;
+    cart.forEach(item => {
+      counter += item.quantity
+    })
+    return counter;
   }
 
   useEffect(() => {
-    console.log(cart)
+    console.log(cart);
   }, [cart]);
 
   return (
     <Router>
       <div className="App">
-        <Nav />
+        <Nav numberOfItems={numberOfItems()} />
         <Switch>
           <Route path="/" exact component={Home} />
           <Route path="/books" exact render={() => <Books books={books} />} />
           <Route
             path="/books/:id"
-            render={() => <BookInfo books={books} addToCart={addToCart} cart={cart}/>}
+            render={() => (
+              <BookInfo books={books} addToCart={addToCart} cart={cart} />
+            )}
           />
-          <Route path="/cart" render={() => <Cart books={books} />} />
+          <Route
+            path="/cart"
+            render={() => (
+              <Cart
+                cart={cart}
+                setCart={setCart}
+                changeQuantity={changeQuantity}
+                removeItem={removeItem}
+              />
+            )}
+          />
         </Switch>
         <Footer />
       </div>
--- src/components/Nav.jsx
+++ src/components/Nav.jsx
@@ -1,9 +1,9 @@
 import React from "react";
 import { FontAwesomeIcon } from "@fortawesome/react-fontawesome";
-import LibraryLogo from '../assets/Library.svg'
+import LibraryLogo from "../assets/Library.svg";
 import { Link } from "react-router-dom";
 
-const Nav = () => {
+const Nav = ({ numberOfItems }) => {
   function openMenu() {
     document.body.classList += " menu--open";
   }
@@ -35,7 +35,9 @@ const Nav = () => {
             <Link to="/cart" className="nav__link">
               <FontAwesomeIcon icon="cart-shopping" />
             </Link>
-            <span className="cart__length">2</span>
+            {numberOfItems > 0 && (
+              <span className="cart__length">{numberOfItems}</span>
+            )}
           </li>
         </ul>
         <div className="menu__backdrop">
@@ -44,13 +46,19 @@ const Nav = () => {
           </button>
           <ul className="menu__links">
             <li className="menu__list">
-              <Link to="/" className="menu__link">Home</Link>
+              <Link to="/" className="menu__link">
+                Home
+              </Link>
             </li>
             <li className="menu__list">
-              <Link to="/books" className="menu__link">Books</Link>
+              <Link to="/books" className="menu__link">
+                Books
+              </Link>
             </li>
             <li className="menu__list">
-              <Link to="/" className="menu__link">Cart</Link>
+              <Link to="/" className="menu__link">
+                Cart
+              </Link>
             </li>
           </ul>
         </div>
